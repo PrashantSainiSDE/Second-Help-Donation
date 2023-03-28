094914599b0a59fd837febf6179d3b9a89ae4c9c
Prince34998 <112163728+Prince34998@users.noreply.github.com>
2023-03-28 22:17:31 +0530

    Latest code

---
+++ src/app/donate/donate.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-donate',
+  templateUrl: './donate.component.html',
+  styleUrls: ['./donate.component.css']
+})
+export class DonateComponent implements OnInit,AfterViewInit {
+
+  constructor(private elementRef :ElementRef ) { }
+
+  ngOnInit() {
+
+  }
+  ngAfterViewInit(){
+    var s = document.createElement("script");
+    var f = document.createElement("form")
+
+    s.src = "https://checkout.razorpay.com/v1/payment-button.js" ;
+    s.setAttribute('data-payment_button_id', 'pl_LU3XG0oYP3YJ4I');
+    s.async;
+    f.style.textAlign = 'center'
+   f.style.marginTop = '100px'
+    f.appendChild(s);
+    this.elementRef.nativeElement.appendChild(f);
+  }
+
+   }
